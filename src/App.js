5459fa60fef7dcb531ed2335b1be98d8b2a6f9c7
unknown <md.ridgeway.work@gmail.com>
2022-02-03 18:51:50 -0600

    Adding Choices

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
   // shuffle cards
   const shuffleCards = () => {
@@ -25,7 +27,10 @@ function App() {
       setTurns(0)
   }
 
-console.log(cards, turns)
+// handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
 
   return (
     <div className="App">
@@ -33,7 +38,11 @@ console.log(cards, turns)
       <button onClick={shuffleCards}>New Game</button>
       <div className='card-grid'>
         {cards.map(card => (
-          <SingleCard key={card.id} card={card}/>
+          <SingleCard
+           key={card.id} 
+           card={card}
+           handleChoice={handleChoice}
+          />
         ))}
       </div>
     </div>
